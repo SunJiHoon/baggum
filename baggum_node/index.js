e7618d39dad7dcf641e6aa9e01c5ef9d3a24a8f3
SunJiHoon <cangogi1@naver.com>
2024-06-29 18:50:22 +0900

    로그인 페이지와 about 페이지 제작

--- baggum_node/index.js
+++ baggum_node/index.js
@@ -1,8 +1,15 @@
 // index.js
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 const port = 30000;
+
+
+// Body parser middleware 설정
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
@@ -14,6 +21,13 @@ const port = 30000;
 
 // 'public' 디렉토리를 정적 파일 서빙 디렉토리로 설정
 app.use(express.static(path.join(__dirname, 'public')));
+
+// 라우트 설정
+const loginRouter = require('./routes/login');
+const aboutRouter = require('./routes/about');
+app.use('/login', loginRouter);
+app.use('/about', aboutRouter);
+
 
 // 기본 라우트 설정
 app.get('/', (req, res) => {

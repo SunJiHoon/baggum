78ffaab4d20eb702462623ab2c9fdcf5e9f6bf53
MlNTYS <128951201+MlNTYS@users.noreply.github.com>
2024-07-04 15:58:17 +0900

    MongoDB 설치+사용법

--- baggum_node/index.js
+++ baggum_node/index.js
@@ -5,6 +5,19 @@ const path = require('path');
 const app = express();
 const port = 30000;
 
+//MongoDB 계정 생성
+//MongoDB 클러스터 생성, 이때 가장 가까우면서 무료인 서버 추천
+//MongoDB 유저 생성, 이때 아이디 비번/비번 기억 + Connection Method는 Application
+//Mongoose, MongoDB를 편하게 가용 가능하게 해주는 툴 인스톨
+//npm install mongoose --save
+
+//App과 DB연결:
+//const mongoose = require('mongoose')
+//mongoose.connect('Connection String 입력'), {
+//  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+//}).then(() => console.log('MongoDB Connected...))
+//  .catch(err => console.log(Error))
+
 
 // Body parser middleware 설정
 app.use(bodyParser.urlencoded({ extended: false }));

b3e5ea23bedab2f83e19e580ca7ffb241b1262a2
SunJiHoon <cangogi1@naver.com>
2024-07-26 13:01:02 +0900

    auth token null 에러 대처

--- baggum_node/server/middleware/auth.js
+++ baggum_node/server/middleware/auth.js
@@ -6,6 +6,11 @@ let auth = (req,res,next) => {
     //클라이언트 쿠키에서 토큰을 가져온다.
     let token = req.cookies.x_auth;
 
+    // 토큰이 존재하지 않을 경우
+    if (!token) {
+        return res.json({ isAuth: false, error: 'No token provided' });
+    }
+
     //토큰을 복호화한 후 유저를 찾는다
     User.findByToken(token, (err, user)=>{
         if(err) throw err;

861cdd2aa0e931d4a3b663ab40b3ab943eaa5349
SunJiHoon <cangogi1@naver.com>
2024-07-26 12:37:10 +0900

    데이터베이스 mongoDB(moongosse)에서 MySql(seqelize)로 변경

--- baggum_node/server/index.js
+++ baggum_node/server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 const { auth } = require('./middleware/auth')
-const { User } = require('./models/User');
+const { sequelize, User } = require('./models/User'); // sequelize 객체 가져오기
 
 
 //application/x-www-form-urlencoded
@@ -25,13 +25,19 @@ const corsOptions = {
 app.use(cors(corsOptions)); // Use the cors middleware with your options
 
 
-const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI,
-    // {
-    //     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-    // }
-).then(() => console.log("MongoDB Connected..."))
-.catch(err => console.log(err))
+// 모델 동기화
+sequelize.sync()
+  .then(() => console.log('Database synced'))
+  .catch(err => console.error('Error syncing database:', err));
+
+
+// const mongoose = require('mongoose');
+// mongoose.connect(config.mongoURI,
+//     // {
+//     //     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+//     // }
+// ).then(() => console.log("MongoDB Connected..."))
+// .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
--- baggum_node/server/models/User.js
+++ baggum_node/server/models/User.js
@@ -1,117 +1,84 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const saltRounds = 10
-const jwt = require("jsonwebtoken")
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const userShema = mongoose.Schema({
-    name:{
-        type: String,
-        maxlength : 50,
-    },
-    email:{
-        type: String,
-        trim: true,
-        unique : 1
-    },
-    password:{
-        type: String,
-        minlength: 5
-    },
-    lastname: {
-        type:String,
-        maxlength: 50
-    },
-    role:{
-        type: Number,
-        default: 0
-    },
-    image : String,
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type: Number
+// MySQL 데이터베이스 연결 설정
+const sequelize = new Sequelize('baggum', 'cangogi1', 'dkagh123!@#A', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+// User 모델 정의
+class User extends Model {
+  async comparePassword(plainPassword) {
+    return bcrypt.compare(plainPassword, this.password);
+  }
+
+  generateToken() {
+    const token = jwt.sign({ id: this.id }, 'secretToken');
+    this.token = token;
+    return this.save();
+  }
+
+  static async findByToken(token) {
+    try {
+      const decoded = jwt.verify(token, 'secretToken');
+      return await User.findOne({ where: { id: decoded.id, token } });
+    } catch (err) {
+      throw err;
     }
-})
-
-
-userShema.pre('save', function(next){
-    var user = this;
-
-    if(user.isModified('password')){
-    //비밀번호를 암호화 시킨다.
-        bcrypt.genSalt(saltRounds, function(err, salt){
-            if(err) return next(err);
-            bcrypt.hash(user.password, salt, function(err, hash){
-                if (err) return next(err);
-                user.password = hash;
-                next();
-            })
-        })
-    }
-    else{
-        next()
-    }
-})
-
-userShema.methods.comparePassword = function(plainPassword, cb){
-    //plainPassword 1234567 암호화된 비밀번호  afpoisidfoasknvv 
-    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
-        if(err) return cb(err);
-        cb(null,isMatch)
-    })
+  }
 }
 
-userShema.methods.generateToken = function(cb){
-    var user = this;
+// User 모델의 속성 정의
+User.init({
+  name: {
+    type: DataTypes.STRING,
+    maxLength: 50,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [5],
+    },
+  },
+  lastname: {
+    type: DataTypes.STRING,
+    maxLength: 50,
+  },
+  role: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  image: {
+    type: DataTypes.STRING,
+  },
+  token: {
+    type: DataTypes.STRING,
+  },
+  tokenExp: {
+    type: DataTypes.INTEGER,
+  }
+}, {
+  sequelize,
+  modelName: 'User',
+  hooks: {
+    beforeSave: async (user) => {
+      if (user.changed('password')) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  }
+});
 
-    // const payload = { userId: user._id }; // 객체 형태로 페이로드를 구성
-    //jsonwebtoken을 이용하여 토큰을 생성하기
-    // var token = jwt.sign(user._id, 'secretToken')
-    // var token = jwt.sign(payload, 'secretToken')
-    var token = jwt.sign(user._id.toHexString(), 'secretToken')
-
-    user.token = token;
-    // user.save(function(err, user){
-    //     if(err) return cb(err);
-    //     cb(null, user)
-    // })
-
-    user.save()
-    .then(savedUser => {
-      cb(null, savedUser); // 저장된 사용자 정보를 콜백으로 전달
-    })
-    .catch(err => {
-      cb(err); // 에러를 콜백으로 전달
-    });
-  
-}
-
-
-userShema.statics.findByToken = function(token,cb){
-    var user = this;
-
-    //토큰을 decode 한다.
-    jwt.verify(token, 'secretToken', function(err, decoded){
-        //유저 아이디를 이용해서 유저를 찾은 다음에
-        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
-        // user.findOne({"_id" : decoded, "token": token }, function(err, user){
-        //     if(err) return cb(err);
-        //     cb(null, user)
-        // })
-
-        user.findOne({"_id" : decoded, "token": token })
-        .then(user=>{
-            cb(null, user)
-        })
-        .catch(err=>{
-            return cb(err);
-        });
-    })
-
-    
-}
-
-const User = mongoose.model('user', userShema)
-
-module.exports = { User }
+module.exports = { User, sequelize }; // sequelize 객체 내보내기

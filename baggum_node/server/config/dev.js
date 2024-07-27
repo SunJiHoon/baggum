e2cef25495594398bebc23ab7ddcb991c5704a14
acidify04 <peh4622@naver.com>
2024-07-27 15:06:50 +0900

    Stop tracking dev.js

--- baggum_node/server/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    mongoURI: "mongodb+srv://cangogi123:CMC6pKwgAQxmtNLd@cluster0.id6qcfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    DB_NAME: 'baggum',
-    DB_USER: 'cangogi1',
-    DB_PASSWORD: 'dkagh123!@#A',
-    DB_HOST: 'localhost',
-    DB_DIALECT: 'mysql',
-}

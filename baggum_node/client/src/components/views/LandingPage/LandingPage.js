0c8e82501a42b9064bb324a791ff99b80d43f532
SunJiHoon <cangogi1@naver.com>
2024-07-28 11:54:56 +0900

    NAV BAR 사용할 수 있게 변경 후, LANDINGPAGE에 적용

--- baggum_node/client/src/components/views/LandingPage/LandingPage.js
+++ baggum_node/client/src/components/views/LandingPage/LandingPage.js
@@ -30,6 +30,8 @@ function LandingPage() {
   }
 
   return (
+    <div>
+            <NavBar />
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '100vh'
@@ -38,6 +40,8 @@ function LandingPage() {
       <button onClick={onClickHandler}>
         로그아웃
       </button>
+    </div>
+
     </div>
   )
 }

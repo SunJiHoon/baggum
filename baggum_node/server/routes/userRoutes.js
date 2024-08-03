88003fbe4e6f2328fc4ed674b25c742d533dd8bd
FYpingping <jbh3059@gmail.com>
2024-08-03 09:15:02 +0900

    fixed logout bug

--- baggum_node/server/routes/userRoutes.js
+++ baggum_node/server/routes/userRoutes.js
@@ -60,6 +60,7 @@ router.get('/auth', auth, (req, res) => {
   });
 });
 
+/*
 router.get('/logout', auth, (req, res) => {
   User.findOneAndUpdate({ _id: req.user._id }, { token: "" })
     .then(user => res.status(200).send({ success: true }))
@@ -68,5 +69,24 @@ router.get('/logout', auth, (req, res) => {
       return res.json({ success: false, err });
     });
 });
+*/
+
+router.get('/logout', auth, async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { id: req.user.id }});
+    
+    if (user) {
+      await user.update({ token: "" });
+      return res.status(200).json({ success: true , message: "로그아웃 성공"});
+    } else {
+      return res.status(404).json({ success: false, message: "사용자를 찾을 수 없습니다." });
+    }
+  } catch (error) {
+    return res.status(500).json({ success: false, message: "로그아웃 실패", error: error.message });
+  }
+});
+
+
+
 
 module.exports = router;

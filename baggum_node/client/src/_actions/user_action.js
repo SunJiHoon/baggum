f21f2d70ed226beffe1b688666325b8e6a0f9a84
SunJiHoon <cangogi1@naver.com>
2024-08-08 16:05:36 +0900

    환경변수 dev.js로 변경

--- baggum_node/client/src/_actions/user_action.js
+++ baggum_node/client/src/_actions/user_action.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { LOGIN_USER, REGISTER_USER, AUTH_USER } from './types'
+import config from '../config/dev'; // config.js 파일의 경로가 현재 파일 기준으로 맞는지 확인하세요
+
 export function loginUser(dataToSubmit){
     console.log(dataToSubmit)
 
     const baseUrl = process.env.REACT_APP_BASE_URL;
-    const request = axios.post(`${baseUrl}/api/users/login`, dataToSubmit, { withCredentials: true })
+    const request = axios.post(`${config.baseUrl}/api/users/login`, dataToSubmit, { withCredentials: true })
     .then(response =>  response.data )
 
     return{
@@ -18,7 +20,7 @@ export function registerUser(dataToSubmit){
     console.log(dataToSubmit)
 
     const baseUrl = process.env.REACT_APP_BASE_URL;
-    const request = axios.post(`${baseUrl}/api/users/register`, dataToSubmit, { withCredentials: true })
+    const request = axios.post(`${config.baseUrl}/api/users/register`, dataToSubmit, { withCredentials: true })
     .then(response =>  response.data )
 
     return{
@@ -30,7 +32,7 @@ export function registerUser(dataToSubmit){
 
 export function auth(){
     const baseUrl = process.env.REACT_APP_BASE_URL;
-    const request = axios.get(`${baseUrl}/api/users/auth`, { withCredentials: true })
+    const request = axios.get(`${config.baseUrl}/api/users/auth`, { withCredentials: true })
     .then(response => response.data)
 
     return{

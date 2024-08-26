d46618d48ec15791b5d59e66949b6caa9416782e
SunJiHoon <cangogi1@naver.com>
2024-08-26 16:53:52 +0900

    격자무늬로 변경 및 padding 추가

--- baggum_node/client/src/components/views/ItemPage/ItemPage.js
+++ baggum_node/client/src/components/views/ItemPage/ItemPage.js
@@ -1,4 +1,4 @@
-import config from '../../../config/dev'; // config.js 파일의 경로가 현재 파일 기준으로 맞는지 확인하세요
+import config from '../../../config/dev';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -20,22 +20,35 @@ function ItemsPage() {
 
   return (
     <div>
-    <NavBar />
+      <NavBar />
 
-
-    <div>
-      <h1>Items</h1>
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>
-            <img src={item.imagePath} alt={item.name} style={{ width: '100px' }} />
-            <Link to={`/items/${item.id}`}>{item.name}</Link>
-            <p>Desired Merchandise: {item.desiredMerchandise}</p>
-          </li>
-        ))}
-      </ul>
-      <Link to="/items/new">Add New Item</Link>
-    </div>
+      <div>
+        {/* <h1>Items</h1> */}
+        <div 
+          style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(6, 1fr)',
+            gap: '20px',
+            padding: '0 40px', // 좌우에 20px 패딩을 추가
+          }}
+        >
+          {items.map(item => (
+            <div 
+              key={item.id} 
+              style={{
+                border: '1px solid #ccc',
+                padding: '10px',
+                textAlign: 'center',
+              }}
+            >
+              <img src={item.imagePath} alt={item.name} style={{ width: '100px' }} />
+              <Link to={`/items/${item.id}`}>{item.name}</Link>
+              <p>Desired Merchandise: {item.desiredMerchandise}</p>
+            </div>
+          ))}
+        </div>
+        <Link to="/items/new">Add New Item</Link>
+      </div>
     </div>
   );
 }

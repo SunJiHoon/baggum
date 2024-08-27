d29ebb27f0b6b26ecd6137249f160d7ab979367b
SunJiHoon <cangogi1@naver.com>
2024-08-27 20:40:56 +0900

    auth 원상복구

--- baggum_node/client/src/components/contexts/AuthContext.js
+++ baggum_node/client/src/components/contexts/AuthContext.js
@@ -9,7 +9,6 @@ export const AuthProvider = ({ children }) => {
     isAuthenticated: false,
     user: null,
     loading: true,
-    isAuthenticatedInChat: false
   });
 
   useEffect(() => {
@@ -20,43 +19,23 @@ export const AuthProvider = ({ children }) => {
           isAuthenticated: response.data.isAuth,
           user: response.data,
           loading: false,
-          isAuthenticatedInChat: authState.isAuthenticatedInChat
         });
       } catch (error) {
         setAuthState({
           isAuthenticated: false,
           user: null,
           loading: false,
-          isAuthenticatedInChat: authState.isAuthenticatedInChat
         });
       }
     };
-    // checkChatRoomAuth();
     checkAuth();
   }, []);
 
-  const checkChatRoomAuth = async (roomId, userId) => {
-    try {
-      const response = await axios.get(`${config.baseUrl}/api/chat/auth/chat`, { params: { roomId, userId }, withCredentials: true });
-      console.log(response);
-      console.log(response.status === 200);
-      
-      setAuthState((prevState) => ({
-        ...prevState,
-        isAuthenticatedInChat: response.status === 200
-      }));
-    } catch (error) {
-      setAuthState((prevState) => ({
-        ...prevState,
-        isAuthenticatedInChat: false
-      }));
-    }
-  };
 
 
 
   return (
-    <AuthContext.Provider value={{ ...authState, checkChatRoomAuth }}>
+    <AuthContext.Provider value={{ ...authState }}>
       {children}
     </AuthContext.Provider>
   );

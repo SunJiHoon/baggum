e910fd24127db774d2990b181eb962cc0cc2730d
SunJiHoon <cangogi1@naver.com>
2024-09-02 15:00:25 +0900

    craco 첫 도 입

--- baggum_node/client/config-overrides.js
+++ baggum_node/client/config-overrides.js
@@ -1,19 +1,13 @@
 // config-overrides.js
 module.exports = function override(config, env) {
-    // Webpack DevServer 설정을 직접 추가
-    config.devServer = {
-      ...config.devServer,
-      port: 3000, // 포트를 3000으로 설정
-      liveReload: true,
-      host: "0.0.0.0",
-      allowedHosts: "all",
-      open: true,
-      client: {
-        overlay: true,
-        webSocketURL: "ws://localhost:3000/ws", // WebSocket URL을 포트 3000으로 설정
-      },
-      compress: true,
-    };
+    if (env === 'development') {
+      config.devServer = {
+        ...config.devServer,
+        client: {
+          ...config.devServer.client,
+          webSocketURL: 'wss://baggumi.com/ws',
+        },
+      };
+    }
     return config;
   };
-  

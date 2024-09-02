51b9a7575d4396dc17e7b0782d95de79b25f8e94
SunJiHoon <cangogi1@naver.com>
2024-09-02 12:52:50 +0900

    appjs에서 chat 관련 로직 다시 추가1

--- baggum_node/client/src/components/views/ChatPage/ChatPage.js
+++ baggum_node/client/src/components/views/ChatPage/ChatPage.js
@@ -1,10 +1,10 @@
-// import React, { useState, useEffect } from 'react';
-// import io from 'socket.io-client';
-// import NavBar from '../NavBar/NavBar';
-// import config from '../../../config/dev'; // config import
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import NavBar from '../NavBar/NavBar';
+import config from '../../../config/dev'; // config import
 
-// //const socket = io('http://localhost:5000');
-// const socket = io(`${config.baseUrl}`);
+//const socket = io('http://localhost:5000');
+const socket = io(`${config.baseUrl}`);
 
 // function ChatPage() {
 //   const [message, setMessage] = useState('');

ed35f0c367b32b889c4fc9ee9c001cf55b107a91
SunJiHoon <cangogi1@naver.com>
2024-09-02 14:07:44 +0900

    rewired기반 빌드에 config 수정 - webpack이 정보 가져올 때 websocket을 활용함

--- baggum_node/client/config-overrides.js
+++ baggum_node/client/config-overrides.js
@@ -1,19 +1,19 @@
 // config-overrides.js
-const { override, addWebpackDevServerConfig } = require('customize-cra');
-
-module.exports = override(
-  addWebpackDevServerConfig({
-    devServer: {
-      port: 3000,
+module.exports = function override(config, env) {
+    // Webpack DevServer 설정을 직접 추가
+    config.devServer = {
+      ...config.devServer,
+      port: 3000, // 포트를 3000으로 설정
       liveReload: true,
       host: "0.0.0.0",
       allowedHosts: "all",
       open: true,
       client: {
         overlay: true,
-        webSocketURL: "ws://0.0.0.0:5000/ws",
+        webSocketURL: "ws://localhost:3000/ws", // WebSocket URL을 포트 3000으로 설정
       },
       compress: true,
-    },
-  })
-);
+    };
+    return config;
+  };
+  
